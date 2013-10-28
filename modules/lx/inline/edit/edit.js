974643b78124e753451eff860f3b3895d27d47eb
Timo Liebetrau <t.liebetrau@litixsoft.de>
2013-10-28 18:29:21 +0100

    fix lxInlineEdit error

--- modules/lx/inline/edit/edit.js
+++ modules/lx/inline/edit/edit.js
@@ -1,6 +1,6 @@
 /*global angular*/
 angular.module('lx.inline.edit', [])
-    .factory('lx.inline.edit', function () {
+    .factory('lxInlineEdit', function () {
         return function () {
             var pub = {},
                 master = {};
