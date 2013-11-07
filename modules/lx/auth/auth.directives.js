ce4f2901fe1010452384395035dbd7fc640c86cb
Sven Bernstein <s.bernstein@litixsoft.de>
2013-11-07 08:55:10 +0100

    auth aufgeräumt

--- modules/lx/auth/auth.directives.js
+++ modules/lx/auth/auth.directives.js
@@ -1,5 +1,5 @@
 /*global angular*/
-angular.module('lx.auth.directives',['lx/auth/tpls/login_popup.html', 'lx/auth/tpls/logout_popup.html'])
+angular.module('lx.auth.directives', ['lx/auth/tpls/auth_popup.html'])
     .directive('lxAuthPopup', function () {
         return {
             restrict: 'E',
@@ -7,26 +7,5 @@ angular.module('lx.auth.directives',['lx/auth/tpls/login_popup.html', 'lx/auth/t
             transclude: true,
             replace: true,
             templateUrl: 'lx/auth/tpls/auth_popup.html'
-        };
-    })
-    .directive('lxLoginPopup', function () {
-        return {
-            restrict: 'E',
-            controller: 'lxAuthLoginCtrl',
-            transclude: false,
-            replace: true,
-            templateUrl: 'lx/auth/tpls/login_popup.html'
-        };
-    })
-    .directive('lxLogoutPopup', function () {
-        return {
-            restrict: 'E',
-            controller: 'lxAuthLoginCtrl',
-            transclude: false,
-            replace: true,
-            scope: {
-                user: '='
-            },
-            templateUrl: 'lx/auth/tpls/logout_popup.html'
         };
     });
