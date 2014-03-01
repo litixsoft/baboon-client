92a28bf4b6a5d6b6980b123d0c82c0281f9c9d10
Timo Liebetrau <t.liebetrau73@gmail.com>
2014-03-01 07:54:39 +0100

    chore(alert): remove tpl test

--- modules/alert/alert.tpl.js
+++
@@ -1,5 +0,0 @@
-angular.module("alert/alert.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("alert/alert.html",
-    "<h2>Ein Alert Template</h2>\n" +
-    "        <h3>FLUPPI</h3>");
-}]);
