80d497698faf556e881184760e53d9310d7c8b9c
Timo Liebetrau <t.liebetrau73@gmail.com>
2014-03-21 12:58:24 +0100

    test(session): create unit tests

--- modules/session/session.js
+++ modules/session/session.js
@@ -7,16 +7,25 @@ angular.module('bbc.session', ['bbc.transport'])
 
         // save key value in session
         pub.setData = function (key, value, callback) {
-            if (arguments.length === 3) {
-                $bbcTransport.emit('api/session/setData', {key: key, value: value}, callback);
+
+            if (arguments.length !== 3) {
+                throw new Error('error: Missing parameters, key, value and callback required');
             }
-            else {
-                $log.error('parameter error, required key, value and callback');
+
+            if (typeof callback !== 'function') {
+                throw new Error('error: Parameter callback must be function type');
             }
+
+            $bbcTransport.emit('api/session/setData', {key: key, value: value}, callback);
         };
 
         // delete key value in session
         pub.deleteData = function (key, callback) {
+
+            if (arguments.length === 0) {
+                throw new Error('error: Missing parameters, callback required');
+            }
+
             var data = {};
 
             if (arguments.length === 1) {
@@ -27,11 +36,20 @@ angular.module('bbc.session', ['bbc.transport'])
                 data = {key: key};
             }
 
+            if (typeof callback !== 'function') {
+                throw new Error('error: Parameter callback must be function type');
+            }
+
             $bbcTransport.emit('api/session/deleteData', data, callback);
         };
 
         // get key value from session
         pub.getData = function (key, callback) {
+
+            if (arguments.length === 0) {
+                throw new Error('error: Missing parameters, callback required');
+            }
+
             var data = {};
 
             if (arguments.length === 1) {
@@ -41,17 +59,31 @@ angular.module('bbc.session', ['bbc.transport'])
                 data = {key: key};
             }
 
+            if (typeof callback !== 'function') {
+                throw new Error('error: Parameter callback must be function type');
+            }
+
             $bbcTransport.emit('api/session/getData', data, callback);
         };
 
         // check session and set activity time
         pub.getLastActivity = function (callback) {
-            $bbcTransport.emit('api/session/getLastActivity', {}, callback);
+
+            if (typeof callback !== 'function') {
+                throw new Error('error: Parameter callback must be function type');
+            }
+
+            $bbcTransport.emit('api/session/getLastActivity', callback);
         };
 
         // check session and set activity time
         pub.setActivity = function (callback) {
-            $bbcTransport.emit('api/session/setActivity', {}, callback);
+
+            if (typeof callback !== 'function') {
+                throw new Error('error: Parameter callback must be function type');
+            }
+
+            $bbcTransport.emit('api/session/setActivity', callback);
         };
 
         return pub;
---
+++ modules/session/session.spec.js
@@ -0,0 +1,186 @@
+'use strict';
+
+describe('bbcSession', function () {
+
+    var session, mockBbcTransport;
+
+    // init module
+    beforeEach(module('bbc.session'));
+
+    describe('factory: $bbcSession', function() {
+
+        beforeEach(function () {
+
+            mockBbcTransport = {
+                emit: function(route, data, callback) {
+
+                    if (arguments.length < 3) {
+                        callback = data;
+                        data = {};
+                    }
+
+                    callback(null, data);
+                }
+            };
+
+            module(function ($provide) {
+                $provide.value('$bbcTransport', mockBbcTransport);
+            });
+
+            inject(function ($injector) {
+                session = $injector.get('$bbcSession');
+            });
+        });
+
+        it('should be initialized correctly ', function () {
+            expect(typeof session).toBe('object');
+            expect(typeof session.setActivity).toBe('function');
+            expect(typeof session.getLastActivity).toBe('function');
+            expect(typeof session.setData).toBe('function');
+            expect(typeof session.getData).toBe('function');
+            expect(typeof session.deleteData).toBe('function');
+        });
+
+        it('setData: should throw an Error when not given parameters', function () {
+            var func = function () {
+                return session.setData();
+            };
+            expect(func).toThrow(new Error('error: Missing parameters, key, value and callback required'));
+        });
+
+        it('setData: should throw an Error when parameter callback is wrong type', function () {
+            var func = function () {
+                return session.setData('key', 'value', {});
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('setData: should be correctly set data ', function (done) {
+
+            session.setData('key', 'value', function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result.key).toBe('key');
+                expect(result.value).toBe('value');
+
+                done();
+            });
+        });
+
+        it('deleteData: should throw an Error when not given parameters', function () {
+            var func = function () {
+                return session.deleteData();
+            };
+            expect(func).toThrow(new Error('error: Missing parameters, callback required'));
+        });
+
+        it('deleteData: should throw an Error when parameter callback is wrong type', function () {
+            var func = function () {
+                return session.deleteData('string');
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('deleteData: should be correctly deleted data when parameter key is given', function (done) {
+
+            session.deleteData('key', function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result.key).toBe('key');
+                done();
+            });
+        });
+
+        it('deleteData: should be correctly deleted data when parameter key is not given', function (done) {
+
+            session.deleteData(function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result).toBeDefined();
+                done();
+            });
+        });
+
+        it('getData: should throw an Error when not given parameters', function () {
+            var func = function () {
+                return session.getData();
+            };
+            expect(func).toThrow(new Error('error: Missing parameters, callback required'));
+        });
+
+        it('getData: should throw an Error when parameter callback is wrong type', function () {
+            var func = function () {
+                return session.getData('string');
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('getData: should be correctly data when parameter key is given', function (done) {
+
+            session.getData('key', function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result.key).toBe('key');
+                done();
+            });
+        });
+
+        it('getData: should be correctly data when parameter key is not given', function (done) {
+
+            session.deleteData(function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result).toBeDefined();
+                done();
+            });
+        });
+
+        it('getLastActivity: should throw an Error when not given parameters', function () {
+            var func = function () {
+                return session.getLastActivity();
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('getLastActivity: should throw an Error when parameter callback is wrong type', function () {
+            var func = function () {
+                return session.getLastActivity({});
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('getLastActivity: should be correctly data', function (done) {
+
+            session.getLastActivity(function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result).toBeDefined();
+                done();
+            });
+        });
+
+        it('setActivity: should throw an Error when not given parameters', function () {
+            var func = function () {
+                return session.setActivity();
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('setActivity: should throw an Error when parameter callback is wrong type', function () {
+            var func = function () {
+                return session.setActivity({});
+            };
+            expect(func).toThrow(new Error('error: Parameter callback must be function type'));
+        });
+
+        it('setActivity: should be correctly data', function (done) {
+
+            session.setActivity(function(error, result) {
+
+                expect(error).toBeNull();
+                expect(result).toBeDefined();
+                done();
+            });
+        });
+    });
+});
